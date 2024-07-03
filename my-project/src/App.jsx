a25fab6df028ae0d21e5b81513b51c782a6031eb
Shameem <shameemalit96@gmail.com>
2024-07-03 06:13:26 +0530

    created my front pratice abstract project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Header from './Components/Header'
+
+
+
+
+function App() {
+
+  return (
+    <div >
+      <Header/>
+      <h1>hi</h1>
+    </div>
+
+  )
+}
+
+export default App
---
+++ my-project/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h1 className='bg-red-600'  >hi new here</h1>
+    </div>
+  )
+}
+
+export default Header
